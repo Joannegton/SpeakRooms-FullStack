1b87b3fb1137177b1dd3bee9b3a1a715d85b142b
joannegton <wtg131998@gmail.com>
2025-03-29 00:21:01 -0300

    feat: add docker

--- backend/src/modules/usuario/usuario.module.ts
+++ backend/src/modules/usuario/usuario.module.ts
@@ -2,10 +2,18 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsuarioController } from './../usuario.controller'
 import { Module } from '@nestjs/common'
 import { OrmConfig } from 'ormConfig'
+import { UsuarioRepositoryImpl } from './infra/repositories/Usuario.repository'
+import { SalvarUsuarioUseCase } from './application/useCases/SalvarUsuario.usecase'
 
 @Module({
     imports: [TypeOrmModule.forRoot({ ...OrmConfig })],
     controllers: [UsuarioController],
-    providers: [],
+    providers: [
+        {
+            provide: 'UsuarioRepository',
+            useClass: UsuarioRepositoryImpl,
+        },
+        SalvarUsuarioUseCase,
+    ],
 })
 export class UsuarioModule {}
